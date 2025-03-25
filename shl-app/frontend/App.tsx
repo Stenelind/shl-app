0c6528e0609b40cde5f03eeb82cdf1419cb674da
Jonas Stenelind <jstenelind@gmail.com>
2025-03-25 17:20:45 +0100

    fix

--- shl-app/frontend/App.tsx
+++ shl-app/frontend/App.tsx
@@ -1,8 +1,6 @@
-// App.tsx
-import React from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
 
-const App: React.FC = () => {
+const App = () => {
   return <AppNavigator />;
 };
 
